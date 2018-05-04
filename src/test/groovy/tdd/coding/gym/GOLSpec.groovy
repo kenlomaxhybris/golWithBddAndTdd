4ef26494c686498cf760307b5de6f175988f6fb0
Ken Lomax <ken.lomax@sap.com>
2018-05-04 10:23:05 +0200

    Lunch talkW

--- src/test/groovy/tdd/coding/gym/GOLSpec.groovy
+++ src/test/groovy/tdd/coding/gym/GOLSpec.groovy
@@ -8,41 +8,6 @@ import spock.lang.Unroll
 class GOLSpec extends Specification {
 
 
-    @Unroll
-    def "A new GOL populated with #xys has #size cells, namely #cells"(){
-        given:
-        def gol = new GOL();
-        gol.populate( xys)
-
-        expect:
-        gol.getPopulationSize()== size
-        gol.allCells.toString() == cells
-
-        where:
-        xys||size||cells
-        "" || 0 || "[]"
-        "0,0" || 1  || "[0,0]"
-        "-1,0, 0,0, 1,0, 1,0" || 3 || "[-1,0, 0,0, 1,0]"
-
-    }
-
-    @Unroll
-    def "A new GOL populated with #xys looks like #display"(){
-        given:
-        def gol = new GOL();
-        gol.populate( xys)
-
-        expect:
-        gol.show(5)== display
-
-        where:
-        xys||display
-        "" || "---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- "
-        "0,0" || "---------- ---------- ---------- ---------- ---------- -----+---- ---------- ---------- ---------- ---------- ---------- "
-        "-1,0, 0,0, 1,0, 1,0" || "---------- ---------- ---------- ---------- ---------- ----+++--- ---------- ---------- ---------- ---------- ---------- "
-
-    }
-
     def "A new GOL populated with '-1,0, 0,0, 1,0, 1,0' evolves to '0,-1, 0,0, 0,1'"(){
         given:
         def evolver = new GOLEvolver();
@@ -72,6 +37,56 @@ class GOLSpec extends Specification {
 
         then:
         oc.toString().contains("+++");
+
+    }
+
+    @Unroll
+    def "A new GOL populated with #xys has #size cells, namely #cells"(){
+        given:
+        def gol = new GOL();
+        gol.populate( xys)
+
+        expect:
+        gol.getPopulationSize()== size
+        gol.allCells.toString() == cells
+
+        where:
+        xys||size||cells
+        "" || 0 || "[]"
+        "0,0" || 1  || "[0,0]"
+        "-1,0, 0,0, 1,0, 1,0" || 3 || "[-1,0, 0,0, 1,0]"
+
+    }
+
+    def "STUBBED  A new GOL populated with '-1,0, 0,0, 1,0, 1,0' evolves to '0,-1, 0,0, 0,1'"(){
+        given:
+        def evolver = Stub(IGOLEvolver)
+        evolver.evolve(_) >> Cell.createCells( "0,-1, 0,0, 0,1")
+        and:
+        def gol = new GOL( evolver )
+        gol.populate("-1,0, 0,0, 1,0, 1,0")
+
+        when:
+        gol.evolve()
+
+        then:
+        gol.allCells.toString() == "[0,-1, 0,0, 0,1]"
+    }
+
+    @Unroll
+    def "A new GOL populated with #xys looks like #display"(){
+        given:"We populate with #xys "
+        def gol = new GOL();
+        gol.populate( xys)
+
+        expect:"The display includes #display  "
+        gol.show(5)== display
+
+        where:
+        xys||display
+        "" || "---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- ---------- "
+        "0,0" || "---------- ---------- ---------- ---------- ---------- -----+---- ---------- ---------- ---------- ---------- ---------- "
+        "-1,0, 0,0, 1,0, 1,0" || "---------- ---------- ---------- ---------- ---------- ----+++--- ---------- ---------- ---------- ---------- ---------- "
 
     }
 
